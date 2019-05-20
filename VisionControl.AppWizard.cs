07cbb6ce0966884055443c47423482129d94fceb
kuojianghu <784818329@qq.com>
2019-05-20 14:00:37 +0800

    Add files via upload

---
+++ VisionControl.AppWizard.cs
@@ -0,0 +1,74 @@
+using System;
+
+namespace VisionControl
+{
+  partial class VisionControl
+  {
+    ///////////////////////// START WIZARD GENERATED
+    // cognex.wizard.globals.begin
+    private const string mVppFilename = @"G:\拉环盖检测\拉环盖体检测V8.0(5.14).vpp";
+    private const string mApplicationName = "拉环盖体检测V8.0(5.14)";
+    private static bool mUsePasswords = false;
+    private static bool mQuickBuildAccess = true;
+    private const string mDefaultAdministratorPassword = "";
+    private const string mDefaultSupervisorPassword = "";
+    private static DateTime mGenerationDateTime = new DateTime(2019,5,14,3,55,19);
+    private static string mGeneratedByVersion = "55.0.0.0";
+    // cognex.wizard.globals.end
+    ///////////////////////// END WIZARD GENERATED
+
+    private void Wizard_FormLoad()
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.formloadactions
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_AttachPropertyProviders()
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.attachpropertyproviders
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_DetachPropertyProviders()
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.detachpropertyproviders
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_EnableControls(bool running)
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.enablecontrols
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_AddJobTabs(System.Collections.ArrayList newPagesList)
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // begin cognex.wizard.addjobtabs
+      switch (mSelectedJob)
+      {
+        case 0:
+          newPagesList.Add(tabPage_Job0_反面检测);
+          break;
+        case 1:
+          newPagesList.Add(tabPage_Job1_正面检测);
+          break;
+      }
+      // end cognex.wizard.addjobtabs
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_UpdateJobResults(int idx, Cognex.VisionPro.ICogRecord result)
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // begin cognex.wizard.updatejobresults
+      // end cognex.wizard.updatejobresults
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+  }
+}
